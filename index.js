1ec653d18452dc12e92d0abd523877964e1e4772
Elysia Gabe <elysiagabe@gmail.com>
2020-01-29 21:01:10 -0800

    Adds answer to Task 2

--- index.js
+++ index.js
@@ -74,7 +74,26 @@ class Person {
 */
 
 class Car {
-
+  constructor (model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank += gallons;
+  }
+  drive(distance) {
+    let maxDistance = this.tank * this.milesPerGallon;
+    if (distance <= maxDistance) {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    } else {
+      this.odometer += maxDistance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+  }
 }
 
 /*
